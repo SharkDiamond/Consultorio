e846f87a29fcb32036da6bf94880b9a4b4ec8c39
Gabriel-Oficina-Bogota <gabrielarispe24@gmail.com>
2021-06-29 20:59:43 -0500

    Se empezo a trabajar en el diseño de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,257 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container,Row,Col} from "reactstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="Container-fluid color p-4">
+
+<div className="row">
+
+<div className="col-lg-8 col-sm-3 col-md-8 color2">
+
+</div>
+
+
+
+<div className="col-lg-4  color3 col-md-4 rounded text-center tamaCitas">
+
+
+<div className=" row m-4 color2 border border-white rounded align-items-center ">
+
+<div className="col-lg-4 col-md-12">
+
+<img width="111px" src="https://scontent.fsju1-1.fna.fbcdn.net/v/t1.6435-9/180329021_4218093848233484_2283269287699736659_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=aiLB_rPNOyQAX-hMVIP&_nc_ht=scontent.fsju1-1.fna&oh=2a007751b6d2cc0b2eeb94761ebf8413&oe=60E06FEF" alt="..." class="borderColorImage rounded-circle m-2 "></img>
+
+
+</div>
+
+<div className="col-lg-8 col-md-12">
+
+<p className="text-white font-weight-bold h4">Hi Gabriel!</p>
+<p className="colorTurquesa font-weight-bold h4">Tiene 4 citas medicas hoy</p>
+
+</div>
+
+
+
+
+</div>
+
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-11" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-11" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-11" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+<div className="row m-5 border-bottom border-white" >
+
+
+<div className="col-11 col-xs-1" >
+
+<p  align="left" className="font-weight-bold text-white">IDP 123 - <span className="colorTurquesa">Gabi Miranda</span> - <span className="colorSalmon">Dolor de cabeza</span></p>
+
+</div>
+
+<div className="col-1 col-xs-1 ">
+  <button   className="btn btn-primary botonComentar ">C</button>
+  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+  </div>
+    
   );
 }
 
