1d31f9ce1eef1e5125db996ce50cd5733296d7ad
Gabriel Arispe <gabrielarispe24@gmail.com>
2021-09-16 21:52:30 -0700

    Se agrego el formulario de busqueda general en la barra, ademas de las opciones de usuario.

--- src/consultorio/src/Barra.js
+++ src/consultorio/src/Barra.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Navbar,Container,Nav,Row,Col  } from 'react-bootstrap';
+import { Navbar,Container,Nav,Row,Col ,FormControl,Button,Form,NavDropdown } from 'react-bootstrap';
 
 import Graficas from "./Iconos/Graficas.png";
 import Paciente from "./Iconos/Paciente.png";
 import Reporte from "./Iconos/Reporte.png";
-
+import './App.css';
 
 class Barra extends Component {
     
@@ -20,8 +20,8 @@ class Barra extends Component {
             <Navbar className="barra pt-3 fixed-top border-bottom border-dark border-2"  expand="lg" >
                        
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-rigth">
-                            <Nav >
+                        <Navbar.Collapse id="responsive-navbar-nav" className="">
+                            <Nav className="me-auto" >
                             
                                <Navbar.Brand href="#link"  onClick={(e) => this.props.update("Graficas")}><img src={Graficas} width="60px" height="60px"  className="img-fluid " /> <span className="h3 fw-bold" style={{color:'#126e82'}} >Graficas</span></Navbar.Brand> 
                                 <Navbar.Brand href="#link"  onClick={(e) => this.props.update("Pacientes")}><img src={Paciente} width="60px" height="60px" className="img-fluid "/><span className="h3 fw-bold" style={{color:'#126e82'}}> Pacientes</span></Navbar.Brand>
@@ -30,6 +30,24 @@ class Barra extends Component {
                               
 
                             </Nav>
+
+                            <Form className="d-flex me-2">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="me-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-dark">Search</Button>
+    </Form>
+
+    <NavDropdown  title={sessionStorage.getItem("Usuario")} id="collasible-nav-dropdown" className="colorNombreUsuario me-5 fw-bold" >
+        
+        <NavDropdown.Item href="#action/3.1">Cambiar Datos</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Cerrar Sesion</NavDropdown.Item>
+        
+      </NavDropdown>
+                        
                         </Navbar.Collapse >
                         
                        
