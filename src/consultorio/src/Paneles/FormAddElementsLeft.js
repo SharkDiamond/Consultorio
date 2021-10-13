a399fcbfcd9e671b081a918d2347937374575ffd
Gabriellaptop <gabrielarispe24@gmail.com>
2021-10-12 21:37:38 -0500

    Se conecto el back end de compromisos y citas ya es posible crearlas desde el fron end

--- src/consultorio/src/Paneles/FormAddElementsLeft.js
+++ src/consultorio/src/Paneles/FormAddElementsLeft.js
@@ -1,36 +1,89 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Form,Row,Col,FormGroup,FormControl,Button } from "react-bootstrap";
-
+import { toast } from 'react-toastify';
 class FormAddElementsLeft extends Component {
     
     
     constructor(props) {
+        
         super(props);
 
         this.state={
 
             dato:"",
-            fecha:""
+            fecha:"",
+            hora:"",
+            IdentificacionEnfermizo:""
 
         };
+
+
         this.bind=this.handleChange.bind(this);
-    }
 
-
-    handleChange=(e)=>{
-
-        this.setState({[e.target.name]:e.target.value});
+        this.bind=this.handleSubmit.bind(this);
 
     }
 
 
-   async handleSubmit(e){
+    handleChange =(e)=>{
+    this.setState({
+
+
+        [e.target.name]:e.target.value
+
+    })
+
+        console.log(this.state.dato);
+
+    }
+
+
+    handleSubmit = async (e)=>{
 
        e.preventDefault();
 
-       const Peticion=await axios.post("localhost:8081/Citas/CreateCita",[]);
+        try {
 
+            if (this.props.type=="Citas") {
+                 
+                let Peticion=await axios.post("http://localhost:8081/Citas/CreateCita",{
+        "IdentificacionEnfermizo":this.state.IdentificacionEnfermizo,
+        "Sintoma":this.state.dato,
+        "Fecha":this.state.fecha
+       },{ headers: {
+        'token': sessionStorage.getItem("Token")
+                }});
+
+             
+      toast.success(Peticion.data.Exito);
+            }
+            
+            else if(this.props.type=="Compromisos") {
+                
+
+                let Peticion=await axios.post("http://localhost:8081/Compromisos/CreateCompromiso",{
+                    "UsuarioPerteneciente":sessionStorage.getItem("Usuario"),
+                    "Nombre":this.state.dato,
+                    "Fecha":this.state.fecha
+                    },{ headers: {
+                            'token': sessionStorage.getItem("Token")
+                        }});
+
+           
+      toast.success(Peticion.data);
+
+
+
+
+            }
+
+
+        } catch (error) {
+
+            toast.warning(error.response.data.Problems[0]);
+             
+        }
 
     }
 
@@ -45,10 +98,10 @@ class FormAddElementsLeft extends Component {
 
             <FormControl placeholder={"Agregar "+this.props.type} name="dato" value={this.state.dato} className="mb-3" onChange={this.handleChange} type="text"/>
             
-           {this.props.type=="Citas" ? <FormControl className="mb-3 "  placeholder={"Identificacion Paciente"} name="IdentificacionEnfermizo" onChange={this.handleChange} type="text" /> : ""} 
+           {this.props.type=="Citas" ? <FormControl className="mb-3 "  placeholder={"Identificacion Paciente"} name="IdentificacionEnfermizo"  value={this.state.IdentificacionEnfermizo}  onChange={this.handleChange} type="text" /> : ""} 
           
-            <FormControl placeholder="Fecha" name="fecha" value={this.state.fecha} onChange={this.handleChange} type="Date" />
-
+            <FormControl placeholder="Fecha" name="fecha" value={this.state.fecha} onChange={this.handleChange} type="date" />
+            <input className="form-control mt-3" type="time" name="hora" value={this.state.hora} onChange={this.handleChange}/>
             <Button type="submit" variant="primary" className="mt-3">Create</Button>
 
         </FormGroup>
